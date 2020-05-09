6a9a21e8b6d0702210981f1dc5d61ac5a443c169
Janyo Carvalho <janyo.medeiros@gmail.com>
2020-05-09 14:23:02 -0300

    projeto criado

---
+++ implementacao/mateus/src/main/java/com/janyo/mateus/MateusApplication.java
@@ -0,0 +1,13 @@
+package com.janyo.mateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateusApplication.class, args);
+	}
+
+}
---
+++ implementacao/mateus/src/test/java/com/janyo/mateus/MateusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janyo.mateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
