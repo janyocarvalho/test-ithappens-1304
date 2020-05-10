e3852cf7bef44d3773f97ee81ca216c704f36759
Janyo Carvalho <janyo.medeiros@gmail.com>
2020-05-10 10:34:57 -0300

    relacionamento entre item pedido, pedido e produto

--- implementacao/mateus/src/main/java/com/janyo/mateus/domain/ItemPedido.java
+++ implementacao/mateus/src/main/java/com/janyo/mateus/domain/ItemPedido.java
@@ -1,29 +1,46 @@
 package com.janyo.mateus.domain;
 
-public class ItemPedido {
-	private Integer id;
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ItemPedidoPK id = new ItemPedidoPK();	
+	
 	private String quantidade;
-	private Integer produtoId;
+	private Double preco;
 	private Integer statusItemId;
-	private Integer pedidoId;
 	
 	public ItemPedido() {
 	}
 
-	public ItemPedido(Integer id, String quantidade, Integer produtoId, Integer statusItemId, Integer pedidoId) {
+	public ItemPedido(Pedido pedido, Produto produto, String quantidade, Double preco, Integer statusItemId) {
 		super();
-		this.id = id;
+		id.setPedido(pedido);
+		id.setProduto(produto);
 		this.quantidade = quantidade;
-		this.produtoId = produtoId;
+		this.preco = preco;
 		this.statusItemId = statusItemId;
-		this.pedidoId = pedidoId;
+	}
+	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
 	}
 
-	public Integer getId() {
+	public ItemPedidoPK getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
 
@@ -35,12 +52,12 @@ public class ItemPedido {
 		this.quantidade = quantidade;
 	}
 
-	public Integer getProdutoId() {
-		return produtoId;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutoId(Integer produtoId) {
-		this.produtoId = produtoId;
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
 	public Integer getStatusItemId() {
@@ -51,14 +68,32 @@ public class ItemPedido {
 		this.statusItemId = statusItemId;
 	}
 
-	public Integer getPedidoId() {
-		return pedidoId;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
-	public void setPedidoId(Integer pedidoId) {
-		this.pedidoId = pedidoId;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 
+	
+	
+	
 }
--- implementacao/mateus/src/main/java/com/janyo/mateus/domain/Pedido.java
+++ implementacao/mateus/src/main/java/com/janyo/mateus/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.janyo.mateus.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -28,6 +31,8 @@ public class Pedido implements Serializable{
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
 	
+	@OneToMany(mappedBy ="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 	}
@@ -70,6 +75,14 @@ public class Pedido implements Serializable{
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -95,6 +108,8 @@ public class Pedido implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
